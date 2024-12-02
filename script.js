e91508d4923181f1ac6252e673dcd3996dc083e1
m-cespa <michele.cespa@gmail.com>
2024-12-02 01:29:56 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    /* target elements, add/remove open class */
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
